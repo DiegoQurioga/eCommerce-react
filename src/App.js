e299d13cd77043c12aa2d4039ffa1411f28c30d2
DiegoQurioga <110609313+DiegoQurioga@users.noreply.github.com>
2022-12-28 19:09:28 -0300

    primer contacto react

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bieo
         </p>
         <a
           className="App-link"
@@ -19,6 +20,8 @@ function App() {
         </a>
       </header>
     </div>
+    </>
+    
   );
 }
 
